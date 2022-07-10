cb443282c64cef70de52edf1865de83b55b59233
SlenderSnell <dtsnell555@gmail.com>
2022-07-10 19:37:49 -0400

    tab functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { useState } from 'react';
 import './App.css';
-import NavItem from './components/NavItem.jsx';
 
 function App() {
+  const [tabSelected, setTabSelected] = useState(0);
   return (
     <>
     <div className="navbar">
       <ul>
-        <NavItem navItemName="To-Do" navItemId="toDoBtn"/>
-        <NavItem navItemName="Contact" navItemId="contactBtn"/>
+        <li><button id="toDoBtn" onClick={() => setTabSelected(0)}>To-Do</button></li>
+        <li><button id="contactBtn" onClick={() => setTabSelected(1)}>Contact</button></li>
       </ul>
     </div>
     <div className="tab-content">
-      <div id="toDoBox" className="active" data-tab-content>
-
-      </div>
-      <div id="contactBox" data-tab-content>
-
-      </div>
+      {tabSelected === 0 ? (
+        <div id="toDoBox">
+          <p>Test 1</p>
+        </div>
+      ) : (
+        <div id="contactBox">
+          <p>Test 2</p>
+        </div>
+      )}
     </div>
     </>
   );
--- src/components/NavItem.jsx
+++
@@ -1,9 +0,0 @@
-function NavItem(props) {
-    return (
-        <>
-        <li><button id={props.navItemId}>{props.navItemName}</button></li>
-        </>
-    )
-}
-
-export default NavItem
